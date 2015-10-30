fa09c3fdff519e59a8f35a847a99c0ee62e6ee02
steventhanna <steventhanna@gmail.com>
2015-10-30 15:30:41 -0400

    Unit tests make me cry

--- test/unit/controllers/BroadcastController.test.js
+++ test/unit/controllers/BroadcastController.test.js
@@ -1,16 +1,31 @@
 // var request = require('supertest');
+// var agent = request.agent;
 //
 // describe('BroadcastController', function() {
 //   describe("#new()", function() {
+//     var user;
+//     before(function(done) {
+//       // user = agent(sails.hook.http.app);
+//       user
+//       agent.post('/create-account')
+//         .send({
+//           username: 'example@email.com',
+//           firstName: 'John',
+//           lastName: 'Smith',
+//           password: 'password',
+//           accessCode: 'gorilla',
+//           displayName: 'John Smith'
+//         })
+//         .expect('200', done);
+//     });
 //     it('should create a new broadcast', function(done) {
 //       request(sails.hooks.http.app)
-//         .post('/broadcast/new')
+//       agent.post('/broadcast/new')
 //         .send({
-//           title: 'Example Title',
-//           embedCode: 'EmbedCode',
-//           date: '11/12/13',
-//           summary: 'This is an example summary statement',
-//           bid: Math.floor(Math.random() * 1000000000000000000000)
+//           title: "Example Title",
+//           date: "2015-10-19",
+//           summary: "This is an example summary statement",
+//           embedCode: "EmbedCode",
 //         })
 //         .expect(200, done)
 //     });
@@ -24,10 +39,8 @@
 //           embedCode: 'EmbedCode',
 //           date: '11/12/13',
 //           summary: 'This is an example summary statement',
-//           bid: Math.floor(Math.random() * 1000000000000000000000)
 //         })
 //         .expect(500, done)
 //     });
 //   });
-//
 // });
